7ac5b2cf6ccef222c13654f9d88145dafaab6d49
Kianosh Pourian <kianoshp@gmail.com>
2018-08-14 13:04:41 -0400

    updated the config file and renamed the send file to message

--- app.js
+++ app.js
@@ -2,36 +2,25 @@
 const http        = require('http')
 const port        = 3000
 const app         = require('express')()
-const creds       = require('./config')
-const client      = require('twilio')(creds.accountSid, creds.authToken)
-const sendMessage = require('./send')
-const MessagingResponse = require('twilio').twiml.MessagingResponse
-const dbConnect         = require('./db')
-const dbClient  = require('mongodb').MongoClient
-const url       = 'mongodb://localhost/FavoritesDB'
+const message     = require('./message')
+// const dbConnect         = require('./db')
+// const dbClient  = require('mongodb').MongoClient
+// const url       = 'mongodb://localhost/FavoritesDB'
 
-
-sendMessage.sendSms(
+message.sendSms(
   '17819623739',
   'this is a test message'
   )
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
-
-  twiml.message('The Robots are coming! Head for the hills!');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-})
+message.receiveSms(app)
 
 // dbConnect.connect();
 
 
-dbClient.connect(url, function(err,db) {
-  console.log('Connected');
-  db.close();
-});
+// dbClient.connect(url, function(err,db) {
+//   console.log('Connected');
+//   db.close();
+// });
 
 app.listen(port, (err) => {
   if (err) {
